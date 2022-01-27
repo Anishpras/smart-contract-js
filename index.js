ef0f1e6bca4a5015376b1882409da4f2519a9b0e
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2022-01-27 21:17:59 +0530

    added gitignore

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-const createContract = () => {
-  console.log("Hello");
+const fs = require("fs");
+const createContract = async (name) => {
+  let data = `
+  // SPDX-License-Identifier: MIT
+// compiler version must be greater than or equal to 0.8.10 and less than 0.9.0
+pragma solidity ^0.8.10;
+
+contract ${name} {
+    string public greet = "Hello World!";
+}
+  `;
+  const dir = "./contracts";
+  if (!fs.existsSync(dir)) {
+    await fs.mkdirSync(dir, {
+      recursive: true,
+    });
+  }
+  await fs.writeFileSync("contracts/Hello.sol", data);
+  console.log("File written successfully\n");
+  console.log("The written has the following contents:");
 };
 
-module.exports = createContract
+createContract("Hello");
+
+module.exports = createContract;
